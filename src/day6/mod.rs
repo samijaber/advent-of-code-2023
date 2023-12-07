014100838b1b18988f06a301d75a37ad577b8286
Sami Jaber <jabersami@gmail.com>
2023-12-07 18:35:10 -0400

    day6-2

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -1,3 +1,5 @@
+use core::panic;
+use itertools::Itertools;
 use std::fs;
 
 pub fn main() {
@@ -6,27 +8,79 @@ pub fn main() {
 
     let data = real_data;
 
+    // let mut lines = data.lines().map(|line| {
+    //     line.split_once(':')
+    //         .unwrap()
+    //         .1
+    //         .trim()
+    //         .split_ascii_whitespace()
+    //         .map(|n| n.trim().parse::<i64>().unwrap())
+    // });
+
+    // let times = lines.next().unwrap();
+    // let records = lines.next().unwrap();
+
+    // let races = times.zip(records);
+
+    // let res = races
+    //     .map(|(t, r)| {
+    //         (1..t)
+    //             .flat_map(move |x| if ((t - x) * x) > r { Some(1) } else { None })
+    //             .count()
+    //     })
+    //     .reduce(|k, l| k * l)
+    //     .unwrap();
+    // println!("{}", res);
+
     let mut lines = data.lines().map(|line| {
         line.split_once(':')
             .unwrap()
             .1
             .trim()
             .split_ascii_whitespace()
-            .map(|n| n.trim().parse::<i64>().unwrap())
+            .join("")
+            .trim()
+            .parse::<i64>()
+            .unwrap()
     });
 
-    let times = lines.next().unwrap();
-    let records = lines.next().unwrap();
+    let time = lines.next().unwrap();
+    let record = lines.next().unwrap();
 
-    let races = times.zip(records);
+    let mut found = false;
+    let mut index = time / 2;
+    let mut step = index;
 
-    let res = races
-        .map(|(t, r)| {
-            (1..t)
-                .flat_map(move |x| if ((t - x) * x) > r { Some(1) } else { None })
-                .count()
-        })
-        .reduce(|k, l| k * l)
-        .unwrap();
-    println!("{}", res);
+    while !found {
+        let is_above = ((time - index) * index) >= record;
+        let prev = index - 1;
+        let below_is_below = ((time - prev) * prev) <= record;
+
+        println!("checking: {}", index);
+
+        println!("{} || {} || {}", ((time - index) * index), record, is_above);
+        println!(
+            "{} || {} || {}",
+            ((time - prev) * prev),
+            record,
+            below_is_below
+        );
+        found = is_above & below_is_below;
+
+        if !found {
+            step = step / 2;
+            println!("index: {} || step: {}", index, step);
+            if is_above && !below_is_below {
+                index = index - step
+            } else {
+                index = index + step
+            }
+            if index == 0 {
+                panic!("something went wrong.");
+            }
+        }
+    }
+
+    let res = time - index - index + 1;
+    println!("res: {}", res);
 }
